882053923951b5a1101f034a7f61e0c2e93b8497
mr-spod <sean.odonnell@duethealth.com>
2017-12-22 18:02:21 -0500

    nm

---
+++ swift/SampleApp/SampleApp/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  SampleApp
+//
+//  Created by Sean O'Donnell on 12/22/17.
+//  Copyright © 2017 Sean O'Donnell. All rights reserved.
+//
+
+import Foundation
+
+class ViewModel: NSObject {
+    
+}
